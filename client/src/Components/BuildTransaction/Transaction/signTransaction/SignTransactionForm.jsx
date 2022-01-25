6c837ef3247e2eeaef15f2e25104db920f045093
aacosta11 <acostaadrian2020@gmail.com>
2022-01-24 19:29:49 -0800

    added labels to SignTransactionForm

--- client/src/Components/BuildTransaction/Transaction/signTransaction/SignTransactionForm.jsx
+++ client/src/Components/BuildTransaction/Transaction/signTransaction/SignTransactionForm.jsx
@@ -1,9 +1,31 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import "../../../../Styles/signTransactionForm.css";
 const SignTransactionForm = props => {
+    const labels = ["signing for","transaction envelope XDR","source account","sequence number","number of operations","number of signatures"]
+    const [dataBoxes,setDataBoxes] = useState();
 
     return (<>
-        <h4>sign dis</h4>
+        <div className="box-grid column-gap-4-px">
+            <div className="G column row-gap-2-px">
+
+                {labels.map((label,i)=>{
+                    return (<>
+                    <div className="X X-ai-center label" key={i}>
+                        <h4>{label}</h4>
+                    </div>
+                    </>)
+                })}
+                
+            </div>
+            <div className="G column row-gap-2-px">
+                {labels.map((l,i)=>{
+                    return (<>
+                    <div className="data"></div>
+                    </>)
+                })}
+            </div>
+
+        </div>
     </>)
 }
 export default SignTransactionForm;
