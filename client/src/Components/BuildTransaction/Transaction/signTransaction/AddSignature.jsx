c52cd053a10661f7d77220d2ebe68b037803363d
aacosta11 <acostaadrian2020@gmail.com>
2022-01-26 18:38:12 -0800

    bug

--- client/src/Components/BuildTransaction/Transaction/signTransaction/AddSignature.jsx
+++ client/src/Components/BuildTransaction/Transaction/signTransaction/AddSignature.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import FilterInputs from "../../../Shared/FilterInputs";
 const AddSignature = props => {
+    const [sigs,setSigs] = useState([]);
+    const {replaceSpecialCharacters} = FilterInputs;
+    const handleSecretKey = e => {
+        let sigsCopy = [...sigs];
+        sigsCopy.push(replaceSpecialCharacters(e.value))
+    }
     return (<>
-        <h4>AddSignature.jsx</h4>
+        <h4>Signatures</h4>
+        <input type="text" onChange={(e)=>handleSecretKey(e)} />
     </>)
 }
 export default AddSignature;
--- client/src/Components/BuildTransaction/Transaction/signTransaction/SignTransactionForm.jsx
+++ client/src/Components/BuildTransaction/Transaction/signTransaction/SignTransactionForm.jsx
@@ -3,29 +3,28 @@ import "../../../../Styles/signTransactionForm.css";
 import AddSignature from "./AddSignature";
 const SignTransactionForm = props => {
     const labels = ["signing for","transaction envelope XDR","source account","sequence number","number of operations","number of signatures"]
-    const [dataBoxes,setDataBoxes] = useState();
+
 
     return (<>
         <div className="box-grid column-gap-4-px">
             <div className="G column row-gap-2-px">
 
                 {labels.map((label,i)=>{
-                    return (<>
+                    return (
                     <div className="X X-ai-center label" key={i}>
                         <h4>{label}</h4>
                     </div>
-                    </>)
+                    )
                 })}
                 
             </div>
             <div className="G column row-gap-2-px">
                 {labels.map((l,i)=>{
-                    return (<>
-                    <div className="data"></div>
-                    </>)
+                    return (
+                    <div className="data" key={i}></div>
+                    )
                 })}
             </div>
-            <AddSignature />
         </div>
     </>)
 }
