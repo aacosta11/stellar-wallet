b7943ec7064d78d9c86acc1ebad99277286c62d6
aacosta11 <acostaadrian2020@gmail.com>
2022-03-01 21:49:35 -0800

    moved components

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 // component to wrap content for home, about, and history
 import React from "react";
-import DashboardLayout from "../Components/Dashboard/DashboardLayout";
-import NavigationLayout from "../Components/Dashboard/Navigation/NavigationLayout";
+import DashboardLayout from "./DashboardLayout";
+import NavigationLayout from "./Navigation/NavigationLayout";
 const Dashboard = () => {
 
     return (<>
--- client/src/Components/Dashboard/Navigation/ListOfActions.jsx
+++ client/src/Components/Dashboard/Navigation/ListOfActions.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 // imports of network interaction components
-import BuildTransaction from "../Content/BuildTransaction/BuildTransaction";
-import SignTransaction from "../Content/SignTransaction/SignTransaction";
-import SubmitTransaction from "../Content/SubmitTransaction/SubmitTransaction";
+import BuildTransaction from "../../NetworkInteraction/interactions/BuildTransaction/BuildTransaction";
+import SignTransaction from "../../NetworkInteraction/interactions/SignTransaction/SignTransaction";
+import SubmitTransaction from "../../NetworkInteraction/interactions/SubmitTransaction/SubmitTransaction";
 
 // object of network interactions (key: network name, value: network interaction component)
 const ListOfActions = {
